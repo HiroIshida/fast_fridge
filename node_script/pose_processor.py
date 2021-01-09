9c8aa0b5c77213b7e8365e5d4a9e7f6d2492e6a6
HiroIshida <spitfire.docomo@gmail.com>
2021-01-09 22:21:53 +0900

    Fix transform

--- node_script/pose_processor.py
+++ node_script/pose_processor.py
@@ -12,6 +12,14 @@ from skrobot.coordinates.math import quaternion2rpy, quaternion2matrix
 from skrobot.coordinates import make_coords, rpy_matrix, matrix2quaternion
 from skrobot.coordinates import rpy_angle
 
+def tf2skcoords(transform):
+    trans, rotation = transform
+    position = [trans[0], trans[1], trans[2]]
+    quaternion = [rotation[3], rotation[0], rotation[1], rotation[2]]
+    mat = quaternion2matrix(quaternion)
+    co = make_coords(pos = position, rot=mat)
+    return co
+
 class PoseProcessor(object):
     def __init__(self):
         topic_name_detection = "/kinect_head/rgb/ObjectDetection"
@@ -19,6 +27,7 @@ class PoseProcessor(object):
         self.sub1 = rospy.Subscriber(topic_name_detection, ObjectDetection, self.cb_object_detection)
         #self.sub2 = rospy.Subscriber(topic_name_amcl, PoseWithCovarianceStamped, self.cb_amcl)
 
+        self.broadcaster = tf.TransformBroadcaster()
         self.listener = tf.TransformListener()
         self.pub = rospy.Publisher('handle_pose', Pose, queue_size=1)
 
@@ -31,13 +40,25 @@ class PoseProcessor(object):
         target_frame  = "base_link"
         source_frame = msg.header.frame_id
         try:
-            tf_camera_to_map = self.listener.lookupTransform(
+            tf_camera_to_base = self.listener.lookupTransform(
                 target_frame, source_frame, rospy.Time(0))
         except:
             print("cannot obtain transform")
             return
-        tf_handle_to_map = utils.convert(tf_handle_to_camera, tf_camera_to_map)
-        self.handle_pose = tf_handle_to_map
+        sktf_camera_to_handle = tf2skcoords(tf_handle_to_camera) # gyaku?
+        sktf_base_to_camera = tf2skcoords(tf_camera_to_base)
+        sktf_base_to_handle = sktf_base_to_camera.transform(sktf_camera_to_handle)
+        pos = sktf_base_to_handle.worldpos()
+        rot = sktf_base_to_handle.worldrot()
+        skquaternion = matrix2quaternion(rot)
+        w, x, y, z = skquaternion
+        quaternion = [x, y, z, w]
+        self.broadcaster.sendTransform(
+                pos, quaternion,
+                rospy.Time.now(),
+                "fridge_handle",
+                "base_link")
+        self.handle_pose = [pos, quaternion]
 
     def publish_handle_pose_msg(self):
         if self.handle_pose is not None:
@@ -84,5 +105,4 @@ if __name__ == '__main__':
         pp.publish_handle_pose_msg()
         pp.relative_fridge_pose()
         time.sleep(0.05)
-
         #rate.sleep()
