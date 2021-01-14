365f09f402e378dcd2c1dbcf0086e46380fa013e
HiroIshida <spitfire.docomo@gmail.com>
2021-01-15 01:02:46 +0900

    split problems

--- demo.py
+++ demo.py
@@ -30,14 +30,6 @@ from sample_from_manifold import ManifoldSampler
 
 from geometry_msgs.msg import Pose
 
-def transform_av_seq(av_seq, _transform3d):
-    # 2dim trans
-    # 1dim rot
-    transform3d = np.array(_transform3d)
-    for av in av_seq:
-        av[-3:-1] += transform3d[-3:-1]
-        av[-1] += transform3d[-1]
-
 def bench(func):
     def wrapper(*args, **kwargs):
         ts = time.time()
@@ -57,103 +49,9 @@ def detailbench(func):
     return wrapper
 
 # initialization stuff
-np.random.seed(0)
+#np.random.seed(0)
 
-class PoseDependentProblem(object):
-    def __init__(self, robot_model, n_wp, k_start, k_end, angle_open=0.8):
-        joint_list = rarm_joint_list(robot_model)
-        cm = ConstraintManager(n_wp, joint_list, robot_model.fksolver, with_base=True)
-        update_fksolver(robot_model)
-
-        angles = np.linspace(0, angle_open, k_end - k_start + 1)
-
-        fridge = Fridge(full_demo=True)
-        sscc = TinyfkSweptSphereSdfCollisionChecker(fridge.sdf, robot_model)
-        sscc_for_initial_trajectory = TinyfkSweptSphereSdfCollisionChecker(fridge.sdf, robot_model)
-        for link in rarm_coll_link_list(robot_model):
-            sscc.add_collision_link(link)
-            sscc_for_initial_trajectory.add_collision_link(link)
-
-        self.cm = cm
-        self.sscc = sscc
-        self.sscc_for_initial_trajectory = sscc_for_initial_trajectory
-
-        self.fridge = fridge
-        self.robot_model = robot_model
-
-        self.joint_list = joint_list
-
-        # problem parameters
-        self.n_wp = n_wp
-        self.k_start = k_start
-        self.k_end = k_end
-        self.angle_open = angle_open
-        self.ftol = 1e-3
-
-        # visualization stuff
-        self.viewer = None
-        self.constraint_viewer = None
-
-        # cache for MPC
-        self.av_seq_cache = None
-        self.fridge_pose_cache = None # 3dim pose
-
-        self.debug_av_seq_init_cache = None
-
-        # cmd ri
-        self.duration = 0.7
-
-    def send_cmd_to_ri(self, ri):
-        self.robot_model.fksolver = None
-        base_pose_seq = self.av_seq_cache[:, -3:]
-
-        full_av_seq = []
-        for av in self.av_seq_cache:
-            set_robot_config(self.robot_model, self.joint_list, av, with_base=True)
-            full_av_seq.append(self.robot_model.angle_vector())
-
-        time_seq = [self.duration]*self.n_wp
-        ri.angle_vector_sequence(full_av_seq, time_seq)
-        ri.move_trajectory_sequence(base_pose_seq, time_seq, send_action=True)
-
-    def load_subsol_cache(self):
-        name = "subsol_cache.dill"
-        with open(name, "rb") as f:
-            data = dill.load(f)
-            av_subseq_cache = data["av_seq_cache"]
-            av_start = get_robot_config(self.robot_model, self.joint_list, with_base=True)
-            self.av_seq_cache = np.vstack([[av_start] * (self.k_start -1), av_subseq_cache])
-            self.fridge_pose_cache = data["fridge_pose_cache"]
-
-    def load_sol_cache(self, name="sol_cache.dill"):
-        with open(name, "rb") as f:
-            data = dill.load(f)
-            self.av_seq_cache = data["av_seq_cache"]
-            self.fridge_pose_cache = data["fridge_pose_cache"]
-
-    def dump_sol_cache(self, name="sol_cache.dill"):
-        assert (self.av_seq_cache is not None)
-        data = {"av_seq_cache": self.av_seq_cache,
-                "fridge_pose_cache": self.fridge_pose_cache}
-        with open(name, "wb") as f:
-            dill.dump(data, f)
-
-    def setup(self):
-        av_start = get_robot_config(self.robot_model, self.joint_list, with_base=True)
-        self.cm.add_eq_configuration(0, av_start, force=True)
-        sdf_list = self.fridge.gen_door_open_sdf_list(
-                self.n_wp, self.k_start, self.k_end, self.angle_open)
-        self.sscc.set_sdf(sdf_list)
-        for idx, pose in self.fridge.gen_door_open_coords(self.k_start, self.k_end, self.angle_open):
-            self.cm.add_pose_constraint(idx, "r_gripper_tool_frame", pose, force=True)
-
-        # add left arm constraint 
-        co_fridge_inside = self.fridge.copy_worldcoords()
-        co_fridge_inside.translate([0.0, 0.0, 1.2])
-        trans = co_fridge_inside.worldpos()
-        #rpy = rpy_angle(co_fridge_inside.worldrot())[0]
-        self.cm.add_pose_constraint(
-            self.n_wp-1, "l_gripper_tool_frame", trans, force=True)
+class FridgeTask(object):
 
     def create_av_init_from_cached_trajectory(self):
         tf_base2fridge_now = self.fridge.copy_worldcoords()
@@ -189,39 +87,31 @@ class PoseDependentProblem(object):
         self.debug_av_seq_init_cache = av_seq_init
         return av_seq_init
 
-    @bench
-    def solve(self, use_sol_cache=False, maxiter=100, only_ik=False):
-        if use_sol_cache:
-            assert (self.av_seq_cache is not None)
-            av_seq_init = self.create_av_init_from_cached_trajectory()
-        else:
-            av_current = get_robot_config(self.robot_model, self.joint_list, with_base=True)
-            sdf_last = self.sscc.sdf[-1]
-            self.sscc_for_initial_trajectory.set_sdf(sdf_last)
-            av_seq_init = self.cm.gen_initial_trajectory(
-                av_init=av_current, collision_checker=self.sscc_for_initial_trajectory)
+    def send_cmd_to_ri(self, ri):
+        self.robot_model.fksolver = None
+        base_pose_seq = self.av_seq_cache[:, -3:]
 
-        if only_ik:
-            set_robot_config(self.robot_model, self.joint_list, av_seq_init[-1], with_base=True)
-            return
+        full_av_seq = []
+        for av in self.av_seq_cache:
+            set_robot_config(self.robot_model, self.joint_list, av, with_base=True)
+            full_av_seq.append(self.robot_model.angle_vector())
 
+        time_seq = [self.duration]*self.n_wp
+        ri.angle_vector_sequence(full_av_seq, time_seq)
+        ri.move_trajectory_sequence(base_pose_seq, time_seq, send_action=True)
 
-        slsqp_option = {'ftol': self.ftol, 'disp': True, 'maxiter': maxiter}
-        res = tinyfk_sqp_plan_trajectory(
-            self.sscc, self.cm, av_seq_init, self.joint_list, self.n_wp,
-            safety_margin=3e-2, with_base=True, slsqp_option=slsqp_option)
+    def load_sol_cache(self, name="sol_cache.dill"):
+        with open(name, "rb") as f:
+            data = dill.load(f)
+            self.av_seq_cache = data["av_seq_cache"]
+            self.fridge_pose_cache = data["fridge_pose_cache"]
 
-        SUCCESS = 0
-        ITER_LIMIT = 9
-        print("status: {0}".format(res.status))
-        if res.status in [SUCCESS]:
-            av_seq = res.x
-            self.av_seq_cache = av_seq
-            self.fridge_pose_cache = self.fridge.copy_worldcoords()
-            print("trajectory optimization completed")
-            return av_seq
-        else:
-            return None
+    def dump_sol_cache(self, name="sol_cache.dill"):
+        assert (self.av_seq_cache is not None)
+        data = {"av_seq_cache": self.av_seq_cache,
+                "fridge_pose_cache": self.fridge_pose_cache}
+        with open(name, "wb") as f:
+            dill.dump(data, f)
 
     def debug_view(self):
         if self.viewer is None:
@@ -277,6 +167,171 @@ class PoseDependentProblem(object):
         self.fridge.newcoords(co)
         self.fridge.reset_angle()
 
+    @bench
+    def solve(self, use_sol_cache=False, maxiter=100, only_ik=False):
+        if use_sol_cache:
+            assert (self.av_seq_cache is not None)
+            av_seq_init = self.create_av_init_from_cached_trajectory()
+        else:
+            av_seq_init = self._create_init_trajectory()
+        if only_ik:
+            set_robot_config(self.robot_model, self.joint_list, av_seq_init[-1], with_base=True)
+            return
+        self.debug_av_seq_init_cache = av_seq_init
+
+
+        slsqp_option = {'ftol': self.ftol, 'disp': True, 'maxiter': maxiter}
+        res = tinyfk_sqp_plan_trajectory(
+            self.sscc, self.cm, av_seq_init, self.joint_list, self.n_wp,
+            safety_margin=3e-2, with_base=True, slsqp_option=slsqp_option)
+
+        SUCCESS = 0
+        ITER_LIMIT = 9
+        print("status: {0}".format(res.status))
+        if res.status in [SUCCESS]:
+            av_seq = res.x
+            self.av_seq_cache = av_seq
+            self.fridge_pose_cache = self.fridge.copy_worldcoords()
+            print("trajectory optimization completed")
+            return av_seq
+        else:
+            return None
+
+
+class ApproachingTask(FridgeTask):
+    def __init__(self, robot_model, n_wp):
+        joint_list = rarm_joint_list(robot_model)
+        cm = ConstraintManager(n_wp, joint_list, robot_model.fksolver, with_base=True)
+        update_fksolver(robot_model)
+        fridge = Fridge(full_demo=False)
+        sscc = TinyfkSweptSphereSdfCollisionChecker(fridge.sdf, robot_model)
+
+        self.cm = cm
+        self.sscc = sscc
+        for link in rarm_coll_link_list(robot_model):
+            sscc.add_collision_link(link)
+
+        self.fridge = fridge
+        self.robot_model = robot_model
+
+        self.joint_list = joint_list
+
+        # problem parameters
+        self.n_wp = n_wp
+        self.ftol = 1e-3
+        self.viewer = None
+
+    def setup(self, av_goal):
+        av_start = get_robot_config(self.robot_model, self.joint_list, with_base=True)
+        self.cm.add_eq_configuration(0, av_start, force=True)
+        self.cm.add_eq_configuration(self.n_wp-1, av_goal, force=True)
+        self.sscc.set_sdf(self.fridge.sdf)
+
+    def _create_init_trajectory(self):
+        av_current = get_robot_config(self.robot_model, self.joint_list, with_base=True)
+        av_seq_init = self.cm.gen_initial_trajectory(av_init=av_current)
+        return av_seq_init
+
+    def vis_sol(self, av_seq=None):
+        if self.viewer is None:
+            self.viewer = skrobot.viewers.TrimeshSceneViewer(resolution=(641, 480))
+            self.viewer.add(self.robot_model)
+            self.viewer.add(self.fridge)
+            self.cv = ConstraintViewer(self.viewer, self.cm)
+            #self.sscc.add_coll_spheres_to_viewer(self.viewer)
+            self.cv.show()
+            self.viewer.show()
+
+        if av_seq is None:
+            assert self.av_seq_cache is not None
+            av_seq = self.av_seq_cache
+
+        for idx in range(self.n_wp):
+            av = av_seq[idx]
+            set_robot_config(self.robot_model, self.joint_list, av, with_base=True)
+            #self.sscc.update_color()
+            self.viewer.redraw()
+            time.sleep(0.6)
+
+
+class DoorOpeningTask(FridgeTask):
+    def __init__(self, robot_model, n_wp, k_start, k_end, angle_open=0.8):
+        joint_list = rarm_joint_list(robot_model)
+        cm = ConstraintManager(n_wp, joint_list, robot_model.fksolver, with_base=True)
+        update_fksolver(robot_model)
+
+        angles = np.linspace(0, angle_open, k_end - k_start + 1)
+
+        fridge = Fridge(full_demo=True)
+        sscc = TinyfkSweptSphereSdfCollisionChecker(fridge.sdf, robot_model)
+        sscc_for_initial_trajectory = TinyfkSweptSphereSdfCollisionChecker(fridge.sdf, robot_model)
+        for link in rarm_coll_link_list(robot_model):
+            sscc.add_collision_link(link)
+            sscc_for_initial_trajectory.add_collision_link(link)
+
+        self.cm = cm
+        self.sscc = sscc
+        self.sscc_for_initial_trajectory = sscc_for_initial_trajectory
+
+        self.fridge = fridge
+        self.robot_model = robot_model
+
+        self.joint_list = joint_list
+
+        # problem parameters
+        self.n_wp = n_wp
+        self.k_start = k_start
+        self.k_end = k_end
+        self.angle_open = angle_open
+        self.ftol = 1e-3
+
+        # visualization stuff
+        self.viewer = None
+        self.constraint_viewer = None
+
+        # cache for MPC
+        self.av_seq_cache = None
+        self.fridge_pose_cache = None # 3dim pose
+
+        self.debug_av_seq_init_cache = None
+
+        # cmd ri
+        self.duration = 0.7
+
+    def load_subsol_cache(self):
+        name = "subsol_cache.dill"
+        with open(name, "rb") as f:
+            data = dill.load(f)
+            av_subseq_cache = data["av_seq_cache"]
+            av_start = get_robot_config(self.robot_model, self.joint_list, with_base=True)
+            self.av_seq_cache = np.vstack([[av_start] * (self.k_start -1), av_subseq_cache])
+            self.fridge_pose_cache = data["fridge_pose_cache"]
+
+    def setup(self):
+        av_start = get_robot_config(self.robot_model, self.joint_list, with_base=True)
+        self.cm.add_eq_configuration(0, av_start, force=True)
+        sdf_list = self.fridge.gen_door_open_sdf_list(
+                self.n_wp, self.k_start, self.k_end, self.angle_open)
+        self.sscc.set_sdf(sdf_list)
+        for idx, pose in self.fridge.gen_door_open_coords(self.k_start, self.k_end, self.angle_open):
+            self.cm.add_pose_constraint(idx, "r_gripper_tool_frame", pose, force=True)
+
+        # add left arm constraint 
+        co_fridge_inside = self.fridge.copy_worldcoords()
+        co_fridge_inside.translate([0.0, 0.0, 1.2])
+        trans = co_fridge_inside.worldpos()
+        #rpy = rpy_angle(co_fridge_inside.worldrot())[0]
+        self.cm.add_pose_constraint(
+            self.n_wp-1, "l_gripper_tool_frame", trans, force=True)
+
+    def _create_init_trajectory(self):
+        av_current = get_robot_config(self.robot_model, self.joint_list, with_base=True)
+        sdf_last = self.sscc.sdf[-1]
+        self.sscc_for_initial_trajectory.set_sdf(sdf_last)
+        av_seq_init = self.cm.gen_initial_trajectory(
+            av_init=av_current, collision_checker=self.sscc_for_initial_trajectory)
+        return av_seq_init
+
     def sample_from_constraint_manifold(self, k_wp, n_sample=3000, eps=0.2):
         # call this after setup problem
         # TODO this method should be inside constraint manager
@@ -331,15 +386,18 @@ def setup_rosnode():
     sub = rospy.Subscriber(topic_name, Pose, cb_pose)
     return (lambda : pose_current["pose"])
 
-def generate_door_opening_trajectories():
+def generate_door_opening_problem(load_cache=False):
     n_wp = 11
     k_start = 1
     k_end = 4
     robot_model = pr2_init()
-    problem = PoseDependentProblem(robot_model, n_wp, k_start, k_end)
+    problem = DoorOpeningTask(robot_model, n_wp, k_start, k_end)
 
     problem.reset_firdge_pose([2.0, 1.5, 0.0])
     problem.setup()
+    if load_cache:
+        problem.load_sol_cache()
+        return problem
 
     N = 10000
     X_start = problem.sample_from_constraint_manifold(0, n_sample=N, eps=0.1)
@@ -354,18 +412,26 @@ def generate_door_opening_trajectories():
         problem.fridge_pose_cache = problem.fridge.copy_worldcoords()
         av_seq_sol = problem.solve(use_sol_cache=True)
         if av_seq_sol is not None:
-            problem.dump_sol_cache("subsol_cache.dill")
-            return
+            problem.dump_sol_cache()
+            return problem
         print("cannot be solved. retry...")
 
 if __name__=='__main__':
-    #problem, av_seq_sol = generate_door_opening_trajectories()
-    get_current_pose = setup_rosnode()
+    robot_model = pr2_init()
+    n_wp = 10
+    problem_open = generate_door_opening_problem(load_cache=True)
+
+    problem_approach = ApproachingTask(robot_model, n_wp)
+    problem_approach.reset_firdge_pose([2.0, 1.5, 0.0])
+    problem_approach.setup(problem_open.av_seq_cache[0])
+    problem_approach.solve()
+
+    """
+    #get_current_pose = setup_rosnode()
     n_wp = 18
     k_start = 8
     k_end = 11
-    robot_model = pr2_init()
-    problem = PoseDependentProblem(robot_model, n_wp, k_start, k_end)
+    problem = DoorOpeningTask(robot_model, n_wp, k_start, k_end)
 
     def create_cache():
         problem.reset_firdge_pose([2.0, 1.5, 0.0])
@@ -384,6 +450,7 @@ if __name__=='__main__':
 
     #generate_door_opening_trajectories()
     #create_cache()
+    """
     """ simulater demo
     create_cache()
     problem.load_sol_cache()
@@ -399,6 +466,7 @@ if __name__=='__main__':
     av_seq = problem.solve(use_sol_cache=True)
     """
 
+    """
     robot_model2 = pr2_init()
     robot_model2.fksolver = None
     ri = skrobot.interfaces.ros.PR2ROSRobotInterface(robot_model2)
@@ -408,6 +476,7 @@ if __name__=='__main__':
     trans, rpy = get_current_pose()
     problem.reset_firdge_pose_from_handle_pose(trans, rpy)
     solve(True)
+    """
 
     """
     problem.send_cmd_to_ri(ri)
