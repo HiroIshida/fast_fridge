16d0d0fc7e0105b49ed54e2e78e8e634d91a162b
HiroIshida <spitfire.docomo@gmail.com>
2021-01-18 14:33:49 +0900

    [tmp] tf manager

--- real_robot_demo.py
+++ real_robot_demo.py
@@ -1,16 +1,65 @@
 from task import *
 import threading
 
+def qv_mult(q1, v1_):
+    length = np.linalg.norm(v1_)
+    v1 = v1_/length
+    v1 = tf.transformations.unit_vector(v1)
+    q2 = list(v1)
+    q2.append(0.0)
+    v_converted = tf.transformations.quaternion_multiply(
+        tf.transformations.quaternion_multiply(q1, q2), 
+        tf.transformations.quaternion_conjugate(q1)
+    )[:3]
+    return v_converted * length
+
+def convert(tf_12, tf_23):
+    tran_12, rot_12 = [np.array(e) for e in tf_12]
+    tran_23, rot_23 = [np.array(e) for e in tf_23]
+
+    rot_13 = tf.transformations.quaternion_multiply(rot_12, rot_23)
+    tran_13 = tran_23 + qv_mult(rot_23, tran_12)
+    return list(tran_13), list(rot_13)
+
+def tf_from_xytheta(xytheta):
+    x, y, theta = xytheta
+    pos = [x, y, theta]
+    rot = rpy_matrix(*[theta, 0, 0])
+    quat = wxyz2xyzw(matrix2quaternion(rot))
+    tf = [pos, quat.tolist()]
+    return tf
+
 class TransformManager(object):
     # TODO this should be define inside setup_rosnode
     def __init__(self):
         self.T_b_to_w = None # 'robot base when cmd is sent' to world transform
         self.T_bdash_to_b = None # current robot base to world transform
-        self.T_obj_to_rdash = None # object to current robot base
+        self.T_obj_to_bdash = None # object to current robot base
+
+        sub_object_pose = rospy.Subscriber(
+                "object_pose", Pose, self._object_pose_callback)
+        self._reset_bdash_to_b()
+
+    def _reset_bdash_to_b(self):
+        self.T_bdash_to_b = [[0, 0, 0.], [0, 0, 0, 1.0]]
+
+    def _object_pose_callback(self, msg):
+        # msg must be posestamped
+        pos = msg.pose.position
+        quat = msg.pose.orientation
+        self.T_obj_to_bdash = [[pos.x, pos.y, pos.z], [quat.x, quat.y, quat.z, quat.w]]
 
     def set_b_to_w(xytheta):
-        # convert it to transform
-        pass
+        # from simulater
+        self.T_b_to_w = tf_from_xytheta(xytheta)
+        self._reset_bdash_to_b()
+
+    def get_object_world_pose(self):
+        assert (self.T_obj_to_bdash is not None)
+        assert (self.T_bdash_to_b is not None)
+        assert (self.T_b_to_w is not None)
+        T_obj_to_w = convert(convert(self.T_obj_to_bdash, self.T_bdash_to_b), self.T_b_to_w)
+        return T_obj_to_w
 
 class FridgeDemo(object):
     def __init__(self):
@@ -39,6 +88,8 @@ class FridgeDemo(object):
         self.handle_pose = None
         sub_handle_pose = rospy.Subscriber(
                 "handle_pose", Pose, self._handle_pose_callback)
+
+        self.tf_manager = TransformManager()
 
     def _handle_pose_callback(self, msg):
         pos_msg = msg.position
