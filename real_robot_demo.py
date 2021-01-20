14c5da076bf5c0292b638659aa93ce719e044002
HiroIshida <spitfire.docomo@gmail.com>
2021-01-20 21:10:25 +0900

    completed tf stuff

--- real_robot_demo.py
+++ real_robot_demo.py
@@ -1,5 +1,9 @@
 from task import *
 import threading
+from nav_msgs.msg import Odometry
+from skrobot.coordinates.math import wxyz2xyzw, matrix2quaternion
+import tf
+
 
 def qv_mult(q1, v1_):
     length = np.linalg.norm(v1_)
@@ -12,6 +16,18 @@ def qv_mult(q1, v1_):
         tf.transformations.quaternion_conjugate(q1)
     )[:3]
     return v_converted * length
+
+def invert_tf(tf_inp):
+    trans, rot = tf_inp
+    rot_ = copy.copy(rot)
+    for i in range(3):
+        rot_[i] *= -1
+    rot_ = rot_/np.linalg.norm(rot_)
+    trans_ = qv_mult(rot_, [-e for e in trans])
+
+    if np.isnan(trans_[0]):
+        trans_ = [0, 0, 0]
+    return [trans_, rot_]
 
 def convert(tf_12, tf_23):
     tran_12, rot_12 = [np.array(e) for e in tf_12]
@@ -88,8 +104,28 @@ class FridgeDemo(object):
         self.handle_pose = None
         sub_handle_pose = rospy.Subscriber(
                 "handle_pose", Pose, self._handle_pose_callback)
+        self.tf_base_to_odom = None
+        self.tf_base_nominal_to_odom = None
+        self.tf_base_nominal_to_world = None
+        self.tf_can_to_world = None
+        sub_odom = rospy.Subscriber("/base_odometry/odom", Odometry, self._odom_callback)
 
-        self.tf_manager = TransformManager()
+        sub_can_pose = rospy.Subscriber('pose_can_to_odom', Pose, self._can_pose_callback)
+
+    def _can_pose_callback(self, msg):
+        if self.tf_base_nominal_to_odom is None:
+            return
+        pos = msg.position
+        rot = msg.orientation
+        tf_can_to_odom = ([pos.x, pos.y, pos.z], [rot.x, rot.y, rot.z, rot.w])
+        tf_can_to_base_nominal = convert(tf_can_to_odom, invert_tf(self.tf_base_nominal_to_odom))
+        tf_can_to_world = convert(tf_can_to_base_nominal, self.tf_base_nominal_to_world)
+        self.tf_can_to_world = tf_can_to_world
+
+    def _odom_callback(self, msg):
+        pos = msg.pose.pose.position
+        rot = msg.pose.pose.orientation
+        self.tf_base_to_odom = ([pos.x, pos.y, pos.z], [rot.x, rot.y, rot.z, rot.w])
 
     def _handle_pose_callback(self, msg):
         pos_msg = msg.position
@@ -167,6 +203,10 @@ class FridgeDemo(object):
         self.ri.angle_vector_sequence(full_av_seq, time_seq)
         self.ri.move_trajectory_sequence(base_pose_seq, time_seq, send_action=True)
 
+        # set transforms for later tf computation
+        self.tf_base_nominal_to_odom = self.tf_base_to_odom
+        self.tf_base_nominal_to_world = tf_from_xytheta(av_seq[0, -3:])
+
     def simulate(self, vis):
         vis.show_task(self.task_approach)
         vis.show_task(self.task_open)
@@ -181,8 +221,5 @@ if __name__=='__main__':
     time.sleep(3)
 
     demo.update_fridge_pose()
-    #is.show_task(demo.task_approach)
     demo.solve_first_phase(send_action=True)
     demo.solve_while_second_phase(send_action=True)
-    #demo._send_cmd(demo.task_reach.av_seq_cache)
-    #demo.simulate(vis)
