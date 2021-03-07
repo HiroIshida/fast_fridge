cbb25acdcb3048374f158ded3354f3fb9a1560f3
HiroIshida <spitfire.docomo@gmail.com>
2021-03-08 04:43:55 +0900

    update plotlib

--- plotlib.py
+++ plotlib.py
@@ -19,8 +19,9 @@ bitvec_whole_positive = np.zeros(N**3, dtype=bool)
 scat3d = lambda X, c:  ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=c)
 
 pts_positive = grid.pts[bitvec_whole_positive]
-ax.scatter(pts_positive[:, 0], pts_positive[:, 1], pts_positive[:, 2])
+#ax.scatter(pts_positive[:, 0], pts_positive[:, 1], pts_positive[:, 2])
 
+vf_tuples = []
 for traj in traj_list:
     clf = traj.classifier
     F, std = clf.predict(grid.pts, return_std=True)
@@ -31,16 +32,26 @@ for traj in traj_list:
     try:
         verts, faces, _, _ = measure.marching_cubes_lewiner(F, 0, spacing=spacing)
         verts = verts + np.atleast_2d(grid.b_min)
-        ax.plot_trisurf(verts[:, 0], verts[:,1], faces, verts[:, 2], color="yellow", alpha=0.3)
+        vf_tuples.append((verts, faces))
+        #ax.plot_trisurf(verts[:, 0], verts[:,1], faces, verts[:, 2], color="yellow", alpha=0.3)
     except:
         pass
 
 pts_positive = grid.pts[bitvec_whole_positive]
 def myscat(P, **kwargs):
     ax.scatter(P[:, 0], P[:, 1], P[:, 2], **kwargs)
+
 bitvec_negative = np.logical_and(bitvec_inside_fridge, ~bitvec_whole_positive)
-myscat(grid.pts[bitvec_whole_positive], c="blue", s=1)
+myscat(grid.pts[bitvec_whole_positive], c="blue", s=2)
 myscat(grid.pts[bitvec_negative], c="red", s=1)
+
+print(len(vf_tuples))
+with_surf = False
+if with_surf:
+    for vf_tuple in vf_tuples:
+        verts, faces =vf_tuple
+        ax.plot_trisurf(verts[:, 0], verts[:,1], faces, verts[:, 2], color="yellow", alpha=0.7)
+
 xlim, ylim, zlim = zip(grid.b_min, grid.b_max)
 ax.set_xlim(xlim)
 ax.set_ylim(ylim)
@@ -49,5 +60,7 @@ ax.set_zlim(zlim)
 ax.set_xlabel("x")
 ax.set_ylabel("y")
 ax.set_zlabel("z")
-
+fig.set_size_inches(9.5, 7.2)
+filename = "feasible_set{0}.png".format("_withsurf" if with_surf else "")
+plt.savefig(filename, format="png", dpi=300)
 plt.show()
